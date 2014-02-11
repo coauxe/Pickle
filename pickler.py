96ef538b5d5acf4c398d312a21248be95d470da1
Andrew <caolegario@gmail.com>
2014-02-11 15:57:28 -0800

    pass

--- pickler.py
+++ pickler.py
@@ -5,6 +5,4 @@ favorite_color = {"lion":"yellow", "kitty": "red"}
 pickle.dump(favorite_color, open("save.p","wb"))
 
 
-fave_color = pickle.load(open("save.p","rb")) 
-fave_color['new_key'] = 'new_value'
-
+fave_colour = pickle.load(open("save.p","rb")) 
